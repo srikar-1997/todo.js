58945d45e9600b0f985529beb4c458daed036650
srikar-1997 <thalladasrikar7@msitprogram.net>
2020-05-10 00:29:49 +0530

    implemented checkbox, web api for add and delete task

---
+++ static/todo-app.js
@@ -0,0 +1,122 @@
+let taskList = []
+
+class Task {
+    constructor(taskId, name, dueDate, isDone) {
+        this.taskId = taskId;
+        this.name = name;
+        this.dueDate = dueDate;
+        this.isDone = isDone;
+    }
+
+    toString() {
+        let htmlText = '<li class="task" ><div>'
+        htmlText += '<div id = '+this.name+'>'+this.name+'</div>'
+        htmlText += this.dueDate
+        htmlText += "  " + '<button onclick="deleteTask(';
+        htmlText += this.taskId;
+        htmlText += ')">Delete</button>';
+        htmlText += '<input type="checkbox" id = '+this.taskId+' onchange="markTask('+this.taskId+')">'
+        htmlText += '</div></li>';
+        return htmlText;
+    }
+}
+
+function render() {
+    const listUI = document.getElementById("todolist")
+    listUI.innerHTML = "";
+    if (taskList.length === 0) listUI.innerHTML = "No tasks todo :-)"
+    taskList.forEach((task) => {
+        listUI.innerHTML += task.toString();
+        if (task.isDone === true) {
+            document.getElementById(task.name).innerHTML = '<s>'+task.name+'</s>'
+        }
+    })
+}
+
+function deleteTask(taskId) {
+    taskList = taskList.filter(
+        (t) => {
+            if(t.taskId != taskId) 
+            return t;
+        }
+    );
+    render()
+    // call a web api to update the database on the server
+    const request = new XMLHttpRequest();
+    const id = taskId
+    request.open('POST', '/api/deleteTask');
+    request.onload = () => { 
+    const data = JSON.parse(request.responseText);
+
+    if (data.success) {
+        document.getElementById("result").innerHTML = "Task deleted from json file"
+    }
+    }
+    const data = new FormData()
+    data.append('id', id)
+    request.send(data)
+    return false
+    // update the DOM
+}
+
+function createTask() {
+    const taskName = document.getElementById("taskName").value;
+    const dueDate = document.getElementById("myDate").value
+    addTask(new Task(Date.now(), taskName, dueDate, false));
+}
+
+function addTask(t) {
+    taskList.push(t)
+    render();
+
+    obj = { 'taskId' : t.taskId,
+            'name' : t.name,
+            'date': t.dueDate,
+            'isDone' : t.isDone}
+    // call a web api to update the database on the server
+    const request = new XMLHttpRequest();
+    request.open('POST', '/api/addTask');
+    request.onload = () => { 
+    const data = JSON.parse(request.responseText);
+
+    if (data.success) {
+        document.getElementById("result").innerHTML = "Task added to json file"
+    }
+    }
+    var data = JSON.stringify(obj)
+    request.send(data)
+    return false
+}
+
+function markTask(taskId) {
+    taskList.forEach((task) => {
+        if (task.taskId === taskId) {
+            task.isDone = true;
+            document.getElementById(task.name).innerHTML = '<s>'+task.name+'</s>'
+        }
+    })
+}
+
+function init() {
+    console.log("init called");
+    // call a web api to retrieve the task list
+    // write a function to send a api request
+    // get the JSON
+    // assign it to taskList
+    // render
+    const request = new XMLHttpRequest();
+    request.open('POST', '/api/taskList');
+    request.onload = () => { 
+    const data = JSON.parse(request.responseText);
+    console.log(data)
+    console.log("init called");
+
+    for (let i = 0; i < data.tasks.length; i++) {
+        taskList.push(data.tasks[i])
+    }
+    render()
+    }
+    return false
+}
+
+init();
